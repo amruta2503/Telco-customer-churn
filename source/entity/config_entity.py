4810751dd5d58bfc959038e297582a83520ad6d7
amruta2503 <142072684+amruta2503@users.noreply.github.com>
2024-03-09 14:27:57 -0800

    data transformation

--- source/entity/config_entity.py
+++ source/entity/config_entity.py
@@ -28,6 +28,10 @@ class PipelineConfig:
         self.train_file_name = constant.TRAIN_FILE_NAME
         self.test_file_name = constant.TEST_FILE_NAME
 
+        self.dt_binary_class_col = constant.DT_BINARY_CLASS_COL
+        self.dt_multi_class_col = constant.DT_MULTI_CLASS_COL
+        self.dt_multi_class_encoder = constant.DT_ENCODER_PATH
+
         self.dv_train_file_path = os.path.join(self.artifact_dir, constant.DV_DIR_NAME)
         self.dv_test_file_path = os.path.join(self.artifact_dir, constant.DV_DIR_NAME)
 
